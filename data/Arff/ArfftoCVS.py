61971c19b7aafcae325ecb240c07fcb77e23c530
Osama Chaudhary <osamac@outlook.com>
2018-11-25 23:59:42 +0500

    added nsl_kdd.py script which will work  as a base for future model training

---
+++ data/Arff/ArfftoCVS.py
@@ -0,0 +1,33 @@
+import os
+
+# Getting all the arff files from the current directory
+files = [arff for arff in os.listdir('.') if arff.endswith(".arff")]
+
+# Function for converting arff list to csv list
+def toCsv(content):
+    data = False
+    header = ""
+    newContent = []
+    for line in content:
+        if not data:
+            if "@attribute" in line:
+                attri = line.split()
+                columnName = attri[attri.index("@attribute")+1]
+                header = header + columnName + ","
+            elif "@data" in line:
+                data = True
+                header = header[:-1]
+                header += '\n'
+                newContent.append(header)
+        else:
+            newContent.append(line)
+    return newContent
+
+# Main loop for reading and writing files
+for file in files:
+    with open(file , "r") as inFile:
+        content = inFile.readlines()
+        name,ext = os.path.splitext(inFile.name)
+        new = toCsv(content)
+        with open(name+".csv", "w") as outFile:
+            outFile.writelines(new)
