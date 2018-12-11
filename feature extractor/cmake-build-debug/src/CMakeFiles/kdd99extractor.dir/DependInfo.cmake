814b287f799956d5789aa31322aab9d5fe240533
osamac <osamac@outlook.com>
2018-12-11 23:37:27 +0500

    added kdd feature extractor project with modified code to this repository

---
+++ feature extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osamac/CLionProjects/Extractor/src/Config.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/Config.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/Conversation.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/Conversation.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/ConversationFeatures.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/ConversationFeatures.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/ConversationReconstructor.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/ConversationReconstructor.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/FeatureUpdaterCount.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/FeatureUpdaterCount.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/FeatureUpdaterTime.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/FeatureUpdaterTime.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/FiveTuple.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/FiveTuple.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IcmpConversation.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IcmpConversation.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IntervalKeeper.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IntervalKeeper.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IpDatagram.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IpDatagram.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IpFragment.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IpFragment.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IpReassembler.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IpReassembler.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IpReassemblyBuffer.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IpReassemblyBuffer.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/IpReassemblyBufferHoleList.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/IpReassemblyBufferHoleList.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/Packet.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/Packet.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/ReferenceCounter.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/ReferenceCounter.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/Sniffer.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/Sniffer.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsEngine.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsEngine.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsPerHost.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsPerHost.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsPerService.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsPerService.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsPerServiceWithSrcPort.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsPerServiceWithSrcPort.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsWindow.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsWindow.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsWindowCount.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsWindowCount.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/StatsWindowTime.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/StatsWindowTime.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/TcpConnection.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/TcpConnection.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/Timestamp.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/Timestamp.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/UdpConversation.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/UdpConversation.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/main.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/main.cpp.o"
+  "/home/osamac/CLionProjects/Extractor/src/net.cpp" "/home/osamac/CLionProjects/Extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/net.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ feature extractor/cmake-build-debug/src/CMakeFiles/kdd99extractor.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kdd99extractor.dir/Config.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/Conversation.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/ConversationFeatures.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/ConversationReconstructor.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/FeatureUpdaterCount.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/FeatureUpdaterTime.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/FiveTuple.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IcmpConversation.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IntervalKeeper.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IpDatagram.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IpFragment.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IpReassembler.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IpReassemblyBuffer.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/IpReassemblyBufferHoleList.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/Packet.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/ReferenceCounter.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/Sniffer.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsEngine.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsPerHost.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsPerService.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsPerServiceWithSrcPort.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsWindow.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsWindowCount.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/StatsWindowTime.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/TcpConnection.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/Timestamp.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/UdpConversation.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/main.cpp.o"
+  "CMakeFiles/kdd99extractor.dir/net.cpp.o"
+  "kdd99extractor.pdb"
+  "kdd99extractor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kdd99extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
