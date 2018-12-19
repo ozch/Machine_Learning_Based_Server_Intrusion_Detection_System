d0b2085b642a398b78f48293db03266b4c4f43fb
osamac <osamac@outlook.com>
2018-12-19 16:46:51 +0500

    going to do something which i'm going to regret later.. but huh

--- flood_data_process.py
+++ flood_data_process.py
@@ -21,7 +21,7 @@
 #in data set
 # count = 22
 # srv_count = 23
-
+import csv
 class DataProcess:
     #TODO there is something wrong with number of flags we're mapping and number of flags that are in db do some more reasearch and solve the problem
     flag_map = {'S0':0,'S1':1,'S2':2,'S3':3,'SH':4,'SF':5,'OTH':6,'REJ':7,'RSTO':8,'RSTR':9,'RSTOS0':10,'RroSTC':11}
@@ -31,27 +31,51 @@ class DataProcess:
         # sample instance "0;tcp;private;S0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;123;6;1.00;1.00;0.00;0.00;0.05;0.07;0.00;255;26;0.10;0.05;0.00;0.00;1.00;1.00;0.00;0.00"
         #instance = "0;tcp;private;S0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;123;6;1.00;1.00;0.00;0.00;0.05;0.07;0.00;255;26;0.10;0.05;0.00;0.00;1.00;1.00;0.00;0.00"
 
-
+        #Todo Writing Data Directly to File from here with class '0' seperate wirte file functionality later
         sr = instance.split(";")
         #print(sr)
         #TODO : Exception handling needed here
         if(sr[1].startswith("icmp")):
+            csv_file = open('data/ICMP.csv', 'a+')
+            df = ['duration', 'src_bytes', 'dst_bytes', 'count', 'srv_count','class']
+            writer = csv.DictWriter(csv_file, fieldnames=df)
             # ICMP FEED : duration,src_bytes,dst_bytes,count,srv_count
             df = {"duration":sr[0],"src_bytes":sr[4],"dst_bytes":sr[5],"count":sr[9],"srv_count":sr[10]}
+            df_temp = df
+            df_temp.update({'class':0})
+            writer.writerow(df_temp)
             protocol = 1
         else:
             if(sr[2].startswith("http")):
+                csv_file = open('data/HTTP.csv', 'a+')
+                df = ['duration','flag','src_bytes', 'dst_bytes', 'urgent', 'wrong_fragment', 'class']
+                writer = csv.DictWriter(csv_file, fieldnames=df)
                 # HTTP FEED : duration,flag,src_bytes,dst_bytes,urgent,wrong_fragment
                 df = {"duration": sr[0],"flag":sr[3] ,"src_bytes": sr[4], "dst_bytes": sr[5],"urgent": sr[8],"wrong_fragment": sr[7]}
+                df_temp = df
+                df_temp.update({'class': 0})
+                writer.writerow(df_temp)
                 protocol=2
                 df["flag"] = self.flag_map[df["flag"]]
             elif(sr[1].startswith("udp")):
+                csv_file = open('data/UDP.csv', 'a+')
+                df = ['duration','src_bytes', 'dst_bytes', 'count', 'srv_count','diff_srv_rate' ,'dst_host_count','dst_host_same_src_port_rate','class']
+                writer = csv.DictWriter(csv_file, fieldnames=df)
                 # UDP FEED :duration,src_bytes,dst_bytes,count,srv_count,diff_srv_rate,dst_host_count,dst_host_same_src_port_rate
                 df = {"duration": sr[0],"src_bytes": sr[4], "dst_bytes": sr[5],"count":sr[9],"srv_count":sr[10],"diff_srv_rate":sr[16],"dst_host_count":sr[18],"dst_host_same_src_port_rate":sr[22]}
+                df_temp = df
+                df_temp.update({'class': 0})
+                writer.writerow(df_temp)
                 protocol = 3
             else:
+                csv_file = open('data/TCP.csv', 'a+')
+                df = ['duration', 'flag', 'src_bytes', 'dst_bytes', 'count', 'srv_count', 'class']
+                writer = csv.DictWriter(csv_file, fieldnames=df)
                 # TCP FEED : duration,flag,src_bytes,dst_bytes,count,srv_count
                 df = {"duration": sr[0], "flag": sr[3], "src_bytes": sr[4], "dst_bytes": sr[5],"count":sr[9],"srv_count":sr[10]}
+                df_temp = df
+                df_temp.update({'class': 0})
+                writer.writerow(df_temp)
                 df["flag"]= self.flag_map[df["flag"]]
                 protocol=4
         list = []
